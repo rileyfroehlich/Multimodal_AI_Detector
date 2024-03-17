90a86819fe443f14cb48a5c6eea769484807a1a9
Riley <rileyfro2@gmail.com>
2024-03-17 11:47:00 -0500

    Page functionality working as intended. TODO: Styling of home page. Add links to linkedin. Import models

--- main.py
+++ main.py
@@ -7,18 +7,12 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from pathlib import Path
 
-app = FastAPI(title="Please upload a file")
+app = FastAPI()
 
 # Stolen from https://stackoverflow.com/questions/61641514/css-not-recognized-when-served-with-fastapi
-app.mount(
-    "/static",
-    StaticFiles(directory=Path(__file__).parent.absolute() / "static"),
-    name="static",
-)
-templates = Jinja2Templates(directory="html")
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
-# HTML form to upload files ##### TODO THIS COULD BE IMPROVED? ##### CAN I ADD CSS?
-upload_form = FileResponse("")
+templates = Jinja2Templates(directory="html")
 
 @app.get("/")
 def home(request: Request):
@@ -27,25 +21,47 @@ def home(request: Request):
     )
 
 @app.post("/upload/")
-async def upload_file(file: UploadFile = File(...)):
+async def upload_file(request: Request, file: UploadFile = File(...)):
     file_type = file.filename.split(".")[-1]  # File extension determines next steps
+
+    if file_type == '':
+        return templates.TemplateResponse(
+            "index.html"
+        )
     
     # Validate file type
     allowed_types = ['txt', 'pdf', 'docx', 'jpg', 'jpeg', 'png', 'heic', 'mp3', 'wav', 'm4a', 'flac']
     if file_type not in allowed_types:
         return {"error": "Invalid file type"}
+    
+    # Process file and calculate percent score (dummy data for demonstration)
+    percent_score = 90
+    file_details = {
+        "filename": file.filename,
+        "file_type": file_type,
+        "file_contents": "base64encodedimagestring"
+    }
+
+    return templates.TemplateResponse(
+        "score.html",
+        {
+            "request": request,
+            "percent_score": percent_score,
+            "file_details": file_details
+        }
+    )
 
     #Text
     if file_type in ["txt", "pdf", ".docx"]:
         contents = await file.read()
-        return {"filename": file.filename, "file_contents": contents.decode()}
+        return {"filename": file.filename, "file_type": "text", "file_contents": contents.decode()}
     #Image
     elif file_type in ["jpg", "jpeg", "png", "heic"]:
         return {"filename": file.filename, "file_type": "image"}
     #Audio
     elif file_type in ["mp3", "wav", "m4a", "flac"]:
         return {"filename": file.filename, "file_type": "audio"}
-    #Other (Throw error)
+    #Other (Throw error) This should not run
     else:
         return {"filename": file.filename, "file_type": "unknown"}
 
