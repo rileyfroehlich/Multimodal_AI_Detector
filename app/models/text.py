c980ec3cdffbbf224e9ff696fbfeb0e676165b17
DanielJKeys <164223825+DanielJKeys@users.noreply.github.com>
2024-04-02 10:27:18 -0400

    Update text.py

--- app/models/text.py
+++ app/models/text.py
@@ -36,10 +36,9 @@ def text_pipeline(text_file, filetype):
     tokenizer.fit_on_texts(text)
     model_path = f'{BASE_DIR}/models_text/text_model_final.keras'
     model = load_model(model_path)
-    sequences = tokenizer.texts_to_sequences(text)
-    padded_sequences = pad_sequences(sequences, maxlen=1000, padding='post')
-    reshaped_data = padded_sequences.reshape(1, padded_sequences.shape[1], 1)
-    confidence = model.predict(reshaped_data)
+    sequence = tokenizer.texts_to_sequences(text)
+    padded_sequence = pad_sequences(sequence, maxlen=1000, padding='post')
+    confidence = model.predict(np.expand_dims(padded_sequence, 0))
     AI_bool = confidence > .5
     if not AI_bool:
      confidence = 1 - confidence
